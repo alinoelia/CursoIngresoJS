a60a44559e64177f9ac8282c14d4e6a9c9e2337a
alinoelia <aliznoelia@gmail.com>
2021-07-05 16:48:52 -0300

    nuevo cambio julio

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,22 +1,8 @@
 /*
 Alizon Gamboa
 Al presionar el  botón, 
-se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"
-*/
-/*
-Ejercicio 1
-*/
+se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla" */
 function mostrar()
 {
-	// alert("Esto funciona de maravilla");
-	// alert();
-	// error Alert();
-	// alert("esto funciona de maravilla"); 
 	alert("Esto funciona de maravilla"); 
 }
-
-/*
-
-
-
-*/
